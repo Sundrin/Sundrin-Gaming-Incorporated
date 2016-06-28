f0b0843d2a5793605cb800b16d5c5964077a70a3
Devin Ritter <ritterdevin@yahoo.com>
2016-06-27 22:49:00 -0400

    files I'm working on

--- Temporary/Possible-Game-Starter/js/main.js
+++
@@ -1,9 +0,0 @@
-var SundrinGame = SundrinGame || {};
-
-SundrinGame.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
-
-SundrinGame.game.state.add('boot', SundrinGame.Boot);
-SundrinGame.game.state.add('preload', SundrinGame.Preload);
-SundrinGame.game.state.add('mainmenu', SundrinGame.MainMenu);
-SundrinGame.game.state.add('game', SundrinGame.Game);
-SundrinGame.game.state.start('boot');
